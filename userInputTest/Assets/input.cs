0c8319244c34f4ab33439594c1176e663f4c7de6
sameergopali <sameergopali99@gmail.com>
2017-02-02 17:26:10 +0545

    input Controller Test

---
+++ userInputTest/Assets/input.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class input : MonoBehaviour,IDragHandler,IPointerUpHandler,IPointerDownHandler {
+
+   // public GameObject icon;
+	// Use this for initialization
+    private Image controller;
+    private Vector3 inputVector;
+	void Start () {
+        controller = GetComponent<Image>();
+       // controller.rectTransform.anchoredPosition = new Vector2(-Screen.width/2,Screen.height/2);
+    }
+
+    public virtual void OnPointerDown(PointerEventData ped){
+       // controller.rectTransform.anchoredPosition = new Vector2(ped.position.x,ped.position.y);
+        Debug.Log(ped.position.x + " " + ped.position.y);
+    }
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+
+    }
+    public virtual void OnDrag(PointerEventData ped)
+    {
+
+    }
+    //public void createIcon(Vector2 touchPos)
+    //{
+    //  //SetActive(true);
+    //    //icon.transform.position = new Vector3(touchPos.x, touchPos.y, 0);
+    //}
+
+    //public void destroyIcon()
+    //{
+    //    icon.SetActive(false);
+    //}
+    void Update()
+    {
+//        Debug.Log(Input.GetTouch(0).position.x + " " + Input.GetTouch(0).position.y);
+
+    }
+}
---
+++ userInputTest/Assets/touchPosition.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchPosition : MonoBehaviour {
+
+	// Use this for initialization
+    private Vector2 pos;
+    private Vector2 inputVector;
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            pos = Input.GetTouch(0).position;
+         //   Debug.Log(pos);
+        }
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {     
+            // Get movement of the finger since last frame
+            Vector2 touchDeltaPosition = Input.GetTouch(0).position-pos;
+            Vector2 normalizeDeltaPos = touchDeltaPosition / 25.0f;
+            normalizeDeltaPos = (normalizeDeltaPos.magnitude > 1) ? normalizeDeltaPos.normalized : normalizeDeltaPos;
+            Debug.Log("normalize:" + normalizeDeltaPos);
+            Debug.Log("origin:"+pos);
+            inputVector = normalizeDeltaPos;
+
+     
+        }
+
+	}
+    public float horizontal()
+
+    {
+        
+            return inputVector.x;
+       
+    }
+    public float vertical()
+    {
+        return inputVector.y;
+    }
+}
