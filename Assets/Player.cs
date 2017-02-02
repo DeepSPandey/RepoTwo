ccfba24938ab53447ebcc8abeafc7d1af6bf976f
deep <pandeydeep9g@gmail.com>
2017-02-02 07:37:22 +0545

    movement with rigidbody

---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    PlayerController playerController;
+    Rigidbody rb;
+
+    public float playerSpeed;
+
+	// Use this for initialization
+	void Start () {
+        playerController = GetComponent<PlayerController>();    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 inputFromPlayer = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = inputFromPlayer.normalized * playerSpeed;
+        playerController.setVelocity(moveVelocity);
+    }
+}
